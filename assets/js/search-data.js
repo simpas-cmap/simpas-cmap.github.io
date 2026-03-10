da5f858f260304b02d8703b9efdb1d715f639241
pmangold <paul.mangold@polytechnique.edu>
2026-03-10 19:12:03 +0000

    Deploying to gh-pages from @ simpas-cmap/simpas-cmap.github.io@f32b70d43f4f950c1c3ae816638a69c462d9219c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/team/";
           },
+        },{id: "nav-seminar",
+          title: "Seminar",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/seminar/";
+          },
         },{id: "nav-agenda",
           title: "Agenda",
           description: "",
